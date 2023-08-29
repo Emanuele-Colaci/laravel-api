acb0452c210502e82afc8c8de1dcb172a18f53d0
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-08-29 18:42:07 +0200

    api con paginate

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        // $projects = Project::all();
+        // $projects = Project::with('type', 'technologies')->get(); //EAGER LOADING SENZA PAGINAZIONE
+        $projects = Project::with('type', 'technologies')->paginate(3); //EAGER LOADING CON PAGINAZIONE
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+}
